b65734892b8f1273644c110568b60f722c3e70ad
gjankyte <gjankyte@gmail.com>
2022-11-15 17:57:00 +0200

    atlikta 8 ir 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        if (this.budget > 100000000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const teemoBeemo = new Movie("teemo beemo", "herobrine", 300e6);
+const starWars = new Movie("Star Wars", "George Lucas", 11e6);
+
+console.log('teemoBeemo ===', teemoBeemo.wasExpensive());
+console.log('starWars ===', starWars.wasExpensive());
